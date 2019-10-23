af2ecf6768b150405319e7878a220e6ff3ebefbe
MatviyO <33325458+MatviyO@users.noreply.github.com>
2019-10-23 17:19:35 +0300

    upload files

---
+++ README.md
@@ -0,0 +1,13 @@
+# task-manager server
+Node.js server - app "Task Manager"
+
+## Development server
+
+Run `node index.js` for a dev server `http://localhost:3000/`.
+
+## Config
+
+**index.js**:
+ 
+ mongoose.connect(`'mongodb://localhost:27017/taskManager'`, {});
+**URL of the MongoDB database**
---
+++ src/index.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const cors = require('cors');
+
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+mongoose.connect('mongodb://link:game123@ds031721.mlab.com:31721/taskmanager', {useNewUrlParser: true});
+mongoose.Promise = require('bluebird');
+
+
+require('./authenticate/auth')(app);
+
+require('./chat/chat-socket')(io);
+require('./boards/boards-socket')(io);
+require('./items/boards-items-socket')(io);
+
+
+http.listen(3000, () => {
+	console.log('Server started on port 3000');
+})
